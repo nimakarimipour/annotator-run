a5055df069f266ab1aebedf79b2b876225c5fead
nimakarimipour <karimipour.nima@gmail.com>
2024-09-23 01:42:29 -0700

    fix error 10

--- src/main/java/com/google/cloud/tools/jib/registry/credentials/RegistryCredentials.java
+++ src/main/java/com/google/cloud/tools/jib/registry/credentials/RegistryCredentials.java
@@ -125,7 +125,7 @@ public class RegistryCredentials {
    */
   @Nullable
   public Authorization getAuthorization(String registry) {
-    if (!has(registry)) {
+    if (credentials.get(registry) == null) {
       return null;
     }
     return credentials.get(registry).authorization;
