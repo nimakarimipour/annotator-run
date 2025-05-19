9e55a3fc709c7f66cdd833ff3668ea14c2e5bdaa
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:37:45 -0700

    fix: 13 - ASSIGN_FIELD_NULLABLE - keysAndValues = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/util/collections/SmallMap.java
+++ src/main/java/com/ibm/wala/util/collections/SmallMap.java
@@ -186,7 +186,9 @@ public class SmallMap<K, V> implements Map<K, V> {
 
   @Override
   public void clear() {
-    keysAndValues = null;
+    if (keysAndValues != null) {
+      Arrays.fill(keysAndValues, null);
+    }
   }
 
   @Override
