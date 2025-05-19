259cbca3a1734019ce13e8ca57ceb1c27567f391
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:41:50 -0700

    Revert "fix: 59 - UNBOX_NULLABLE - return (t1 - t2); - unboxing of a @Nullable value"
    This reverts commit 8504eb007755ae2037525d383b9e92455285499a.

--- src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
@@ -125,14 +125,13 @@ public class DFS {
 
     @Override
     public int compare(T o1, T o2) {
+      // throws an exception if either argument is not a Node object
       if (o1 == o2) {
         return 0;
       }
       Integer t1 = order.get(o1);
       Integer t2 = order.get(o2);
-      if (t1 == null || t2 == null) {
-        throw new NullPointerException("One of the nodes has not been ordered");
-      }
+      // throws an exception if either node has not been ordered
       return (t1 - t2);
     }
   }
