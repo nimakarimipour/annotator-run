f217d434e7ed060d54a2824ba27470c19f771694
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:38:37 -0700

    Revert "fix: 22 - ASSIGN_FIELD_NULLABLE - this.rhs = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit bba30a470f56497c1b963e075d471a2426bd4443.

--- src/main/java/com/ibm/wala/fixedpoint/impl/GeneralStatement.java
+++ src/main/java/com/ibm/wala/fixedpoint/impl/GeneralStatement.java
@@ -79,7 +79,7 @@ public abstract class GeneralStatement<T extends IVariable<T>>
     }
     this.operator = operator;
     this.lhs = lhs;
-    this.rhs = makeRHS(0); // Replace null with an empty array or collection
+    this.rhs = null;
     this.hashCode = makeHashCode();
   }
 
