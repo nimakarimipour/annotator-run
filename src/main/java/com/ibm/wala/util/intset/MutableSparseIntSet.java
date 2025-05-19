b1d186df9bfd6b14c7ddf4d97adc9483437201dd
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:42:01 -0700

    Revert "fix: 61 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 7ed04132c1f5bec14c4c83600fd05d758ed7ba67.

--- src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
@@ -158,18 +158,20 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
    */
   @Override
   @SuppressWarnings("unused")
-  public void copySet(IntSet that) throws IllegalArgumentException {
+  public void copySet(@Nullable IntSet that) throws IllegalArgumentException {
     if (that == null) {
       throw new IllegalArgumentException("that == null");
     }
     if (that instanceof SparseIntSet) {
       SparseIntSet set = (SparseIntSet) that;
       if (set.elements != null) {
+        // SJF: clone is performance problem. don't use it.
+        // elements = set.elements.clone();
         elements = new int[set.elements.length];
         System.arraycopy(set.elements, 0, elements, 0, set.size);
         size = set.size;
       } else {
-        elements = new int[0]; // Initialize with an empty array instead of null
+        elements = null;
         size = 0;
       }
     } else {
