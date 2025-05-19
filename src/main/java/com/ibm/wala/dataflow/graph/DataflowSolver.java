839f93234aada4df978023b762f0d7cd0802db88
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:41:28 -0700

    fix: 55 - RETURN_NULLABLE - return v; - returning @Nullable expression from method with @NonNull return type

--- src/main/java/com/ibm/wala/dataflow/graph/DataflowSolver.java
+++ src/main/java/com/ibm/wala/dataflow/graph/DataflowSolver.java
@@ -85,7 +85,9 @@ public abstract class DataflowSolver<T, V extends IVariable<V>> extends DefaultF
   public V getOut(Object node) {
     assert node != null;
     V v = node2Out.get(node);
-    assert v != null : "no out set for " + node;
+    if (v == null) {
+      throw new IllegalStateException("no out set for " + node);
+    }
     return v;
   }
 
