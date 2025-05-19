02cb97c65265d0747eb3f064139b76f111509e3f
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:38:24 -0700

    fix: 20 - RETURN_NULLABLE - return predecessors.getRelated(nodeManager.getNumber(node)); - returning @Nullable expression from method with @NonNull return type

--- src/main/java/com/ibm/wala/util/graph/impl/SparseNumberedEdgeManager.java
+++ src/main/java/com/ibm/wala/util/graph/impl/SparseNumberedEdgeManager.java
@@ -145,7 +145,12 @@ public final class SparseNumberedEdgeManager<T> implements NumberedEdgeManager<T
     if (nodeManager.getNumber(node) < 0) {
       throw new IllegalArgumentException("Node not in graph " + node);
     }
-    return predecessors.getRelated(nodeManager.getNumber(node));
+    IntSet result = predecessors.getRelated(nodeManager.getNumber(node));
+    if (result == null) {
+      return EmptyIntSet.instance;
+    } else {
+      return result;
+    }
   }
 
   /**
