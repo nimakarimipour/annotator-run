73a15040c0c38c68d7363c0a5d672623d0f855cb
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:37:26 -0700

    Revert "fix: 9 - DEREFERENCE_NULLABLE - I result = inner.next(); - dereferenced expression inner is @Nullable"
    This reverts commit 9329589740c075c142232dc7184c75b01f756e23.

--- src/main/java/com/ibm/wala/util/collections/ComposedIterator.java
+++ src/main/java/com/ibm/wala/util/collections/ComposedIterator.java
@@ -50,9 +50,6 @@ public abstract class ComposedIterator<O, I> implements Iterator<I> {
 
   @Override
   public I next() {
-    if (inner == null) {
-      throw new NoSuchElementException();
-    }
     I result = inner.next();
     if (!inner.hasNext()) {
       advanceOuter();
