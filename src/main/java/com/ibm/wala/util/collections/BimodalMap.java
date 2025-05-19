61e2135bec3dc7070e80d4ef883a3492b81ae707
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:36:59 -0700

    fix: 5 - DEREFERENCE_NULLABLE - backingStore = HashMapFactory.make(2 * S.size()); - dereferenced expression S is @Nullable

--- src/main/java/com/ibm/wala/util/collections/BimodalMap.java
+++ src/main/java/com/ibm/wala/util/collections/BimodalMap.java
@@ -88,10 +88,11 @@ public class BimodalMap<K, V> implements Map<K, V> {
 
   /** Switch backing implementation from a SmallMap to a HashMap */
   private void transferBackingStore() {
-    assert backingStore instanceof SmallMap;
-    SmallMap<K, V> S = (SmallMap<K, V>) backingStore;
-    backingStore = HashMapFactory.make(2 * S.size());
-    backingStore.putAll(S);
+    if (backingStore instanceof SmallMap) {
+      SmallMap<K, V> S = (SmallMap<K, V>) backingStore;
+      backingStore = HashMapFactory.make(2 * S.size());
+      backingStore.putAll(S);
+    }
   }
 
   /**
