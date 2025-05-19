71824ef56a6796fcaa8758beccdbdaad9fcdef74
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:37:39 -0700

    Revert "fix: 12 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.ibm.wala.util.collections.IVector.get(int) returns @NonNull"
    This reverts commit 0b17940907b028d6af87deb518bc7d6cbba8f60c.

--- src/main/java/com/ibm/wala/util/collections/SparseVector.java
+++ src/main/java/com/ibm/wala/util/collections/SparseVector.java
@@ -50,12 +50,13 @@ public class SparseVector<T> implements IVector<T>, Serializable {
   /**
    * @see com.ibm.wala.util.intset.IntVector#get(int)
    */
+  @Nullable
   @Override
   @SuppressWarnings("unchecked")
   public T get(int x) {
     int index = indices.getIndex(x);
     if (index == -1) {
-      throw new IllegalStateException("No element found at specified index");
+      return null;
     } else {
       return (T) data[index];
     }
