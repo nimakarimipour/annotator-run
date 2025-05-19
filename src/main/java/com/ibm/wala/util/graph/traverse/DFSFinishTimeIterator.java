68a29eebbc03fffecbdfae3c448e90237ae6176a
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:38:47 -0700

    fix: 24 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field theNextElement (line 34) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- src/main/java/com/ibm/wala/util/graph/traverse/DFSFinishTimeIterator.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFSFinishTimeIterator.java
@@ -44,7 +44,10 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
   protected void init(Graph<T> G, Iterator<? extends T> nodes) {
     this.G = G;
     roots = nodes;
-    if (roots.hasNext()) theNextElement = roots.next();
+    theNextElement = null;
+    if (roots.hasNext()) {
+      theNextElement = roots.next();
+    }
   }
 
   private boolean empty() {
