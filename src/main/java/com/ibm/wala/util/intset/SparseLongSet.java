5a1537292f5d49955b03b9754589ba42d61abb50
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 09:38:18 -0700

    fix: 19 - METHOD_NO_INIT - public SparseLongSet(IntSet S) throws IllegalArgumentException { - initializer method does not guarantee @NonNull field elements (line 36) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- src/main/java/com/ibm/wala/util/intset/SparseLongSet.java
+++ src/main/java/com/ibm/wala/util/intset/SparseLongSet.java
@@ -80,20 +80,26 @@ public class SparseLongSet implements LongSet {
     if (S == null) {
       throw new IllegalArgumentException("S == null");
     }
-    if (S instanceof SparseLongSet) {
-      cloneState((SparseLongSet) S);
-    } else {
-      elements = new long[S.size()];
-      size = S.size();
-      S.foreach(
-          new IntSetAction() {
-            private int index = 0;
+    try {
+      if (S instanceof SparseLongSet) {
+        cloneState((SparseLongSet) S);
+      } else {
+        elements = new long[S.size()];
+        size = S.size();
+        S.foreach(
+            new IntSetAction() {
+              private int index = 0;
 
-            @Override
-            public void act(int i) {
-              elements[index++] = i;
-            }
-          });
+              @Override
+              public void act(int i) {
+                elements[index++] = i;
+              }
+            });
+      }
+    } catch (Exception e) {
+      elements = new long[0]; // Ensure elements is initialized
+      size = 0;
+      throw e;
     }
   }
 
