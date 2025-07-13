63c2a39edb5dddf13750a66ff3abc31293a74257
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:29:47 -0700

    Revert "fix: 32 - FIELD_NO_INIT - private static MutableLongSetFactory defaultLongSetFactory; - @NonNull static field defaultLongSetFactory not initialized"
    This reverts commit a180aad1be4565bf3525d3e34819ecd743c2ca58.

--- src/main/java/com/ibm/wala/util/intset/LongSetUtil.java
+++ src/main/java/com/ibm/wala/util/intset/LongSetUtil.java
@@ -20,7 +20,7 @@ public class LongSetUtil {
   public static final String INT_SET_FACTORY_CONFIG_PROPERTY_NAME =
       "com.ibm.wala.mutableLongSetFactory";
 
-  private static MutableLongSetFactory defaultLongSetFactory = new MutableSparseLongSetFactory();
+  private static MutableLongSetFactory defaultLongSetFactory;
 
   static {
     MutableLongSetFactory defaultFactory = new MutableSparseLongSetFactory();
