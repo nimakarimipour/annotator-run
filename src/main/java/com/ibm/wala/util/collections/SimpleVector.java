9f7cb7b9b9b5e916ea86241332797ebad8f28830
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:44:03 -0700

    before:41to40 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/collections/SimpleVector.java:43       return null;

--- src/main/java/com/ibm/wala/util/collections/SimpleVector.java
+++ src/main/java/com/ibm/wala/util/collections/SimpleVector.java
@@ -31,18 +31,16 @@ public class SimpleVector<T> implements IVector<T> {
   /**
    * @see com.ibm.wala.util.intset.IntVector#get(int)
    */
-  @Override
-  @SuppressWarnings("unchecked")
   public T get(int x) {
-    if (x < 0) {
-      throw new IllegalArgumentException("illegal x: " + x);
+      if (x < 0) {
+        throw new IllegalArgumentException("illegal x: " + x);
+      }
+      if (x < store.length && store[x] != null) {
+        return (T) store[x];
+      } else {
+        throw new IllegalStateException("No non-null element at index: " + x);
+      }
     }
-    if (x < store.length) {
-      return (T) store[x];
-    } else {
-      return null;
-    }
-  }
 
   /**
    * @see com.ibm.wala.util.intset.IntVector#set(int, int)
