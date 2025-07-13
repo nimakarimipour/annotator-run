dcb0c82b8325941f13ebd4fdfbb36ba91e8d3ab4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:28:22 -0700

    Revert "fix: 18 - DEREFERENCE_NULLABLE - while (roots.hasNext()) { - dereferenced expression roots is @Nullable"
    This reverts commit ed0e84f2f80298002005873588000669d1b67462.

--- src/main/java/com/ibm/wala/util/graph/traverse/DFSDiscoverTimeIterator.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFSDiscoverTimeIterator.java
@@ -76,32 +76,37 @@ public abstract class DFSDiscoverTimeIterator<T> extends ArrayList<T> implements
       throw new NoSuchElementException();
     }
 
+    // we always return the top node on the stack.
     T toReturn = peek();
 
+    // compute the next node to return.
     assert getPendingChildren(toReturn) != null;
     do {
       T stackTop = peek();
       for (T child : Iterator2Iterable.make(getPendingChildren(stackTop))) {
         if (getPendingChildren(child) == null) {
+          // found a new child.
           visitEdge(stackTop, child);
           setPendingChildren(child, getConnected(child));
           push(child);
           return toReturn;
         }
       }
+      // the following saves space by allowing the original iterator to be GCed
       Iterator<T> empty = EmptyIterator.instance();
       setPendingChildren(stackTop, empty);
+      // didn't find any new children. pop the stack and try again.
       pop();
+
     } while (!empty());
 
-    if (roots != null) {
-      while (roots.hasNext()) {
-        T nextRoot = roots.next();
-        if (getPendingChildren(nextRoot) == null) {
-          push(nextRoot);
-          setPendingChildren(nextRoot, getConnected(nextRoot));
-          return toReturn;
-        }
+    // search for the next unvisited root.
+    while (roots.hasNext()) {
+      T nextRoot = roots.next();
+      if (getPendingChildren(nextRoot) == null) {
+        push(nextRoot);
+        setPendingChildren(nextRoot, getConnected(nextRoot));
+        return toReturn;
       }
     }
 
