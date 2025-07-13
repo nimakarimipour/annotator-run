f802921bc47917f2abff1c5e65c594e8926e17f7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:33:44 -0700

    Revert "fix: 70 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 98e614e8aa4cebdc139c955ae51b1b7b4335258f.

--- src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
@@ -217,18 +217,19 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
     if (this.isEmpty()) {
       return;
     } else if (that.isEmpty()) {
-      elements = new int[0]; // Assign an empty array instead of null
+      elements = null;
       size = 0;
       return;
     } else if (this.equals(that)) {
       return;
     }
 
+    // some simple optimizations
     if (size == 1) {
       if (that.contains(elements[0])) {
         return;
       } else {
-        elements = new int[0]; // Assign an empty array instead of null
+        elements = null;
         size = 0;
         return;
       }
@@ -242,7 +243,7 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
         elements[0] = that.elements[0];
         return;
       } else {
-        elements = new int[0]; // Assign an empty array instead of null
+        elements = null;
         size = 0;
         return;
       }
@@ -254,19 +255,20 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
     int[] br = that.elements;
     int bi = 0;
     int bl = that.size;
-    int[] cr = null;
+    int[] cr = null; // allocate on demand
     int ci = 0;
 
     while (ai < al && bi < bl) {
       int cmp = (ar[ai] - br[bi]);
 
-      if (cmp > 0) {
+      // (accept element only on a match)
+      if (cmp > 0) { // a greater
         bi++;
-      } else if (cmp < 0) {
+      } else if (cmp < 0) { // b greater
         ai++;
       } else {
         if (cr == null) {
-          cr = new int[al];
+          cr = new int[al]; // allocate enough (i.e. too much)
         }
         cr[ci++] = ar[ai];
         ai++;
@@ -274,8 +276,9 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
       }
     }
 
+    // now compact cr to 'just enough'
     size = ci;
-    elements = cr == null ? new int[0] : cr;
+    elements = cr;
     return;
   }
 
