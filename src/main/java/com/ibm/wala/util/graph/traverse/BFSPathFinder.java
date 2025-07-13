fbfb88b611c127a4aa88ee32323fe06e449562d4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:32:23 -0700

    Revert "fix: 57 - ASSIGN_FIELD_NULLABLE - private HashMap<Object, T> history = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 609c68357c9a87602752c405435f5fda9a6fdb02.

--- src/main/java/com/ibm/wala/util/graph/traverse/BFSPathFinder.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/BFSPathFinder.java
@@ -138,7 +138,7 @@ public class BFSPathFinder<T> {
   }
 
   @Nullable private ArrayDeque<T> Q = null;
-  private HashMap<Object, T> history = HashMapFactory.make();
+  private HashMap<Object, T> history = null;
 
   /**
    * @return a List of nodes that specifies the first path found from a root to a node accepted by
