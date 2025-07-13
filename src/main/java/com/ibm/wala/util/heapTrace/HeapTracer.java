8fb33a0a1a2b7f1571feae5231dc51e8dd07c398
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:30:22 -0700

    fix: 37 - UNBOX_NULLABLE - result.append(bytes / I).append("   "); - unboxing of a @Nullable value - I

--- src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
+++ src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
@@ -576,10 +576,12 @@ public class HeapTracer {
       for (Object key : sorted) {
         Integer I = instanceCount.get(key);
         Integer bytes = sizeCount.get(key);
-        result.append("  ").append(I).append("   ").append(bytes).append("   ");
-        result.append(bytes / I).append("   ");
-        result.append(key);
-        result.append('\n');
+        if (I != null && I != 0 && bytes != null) {
+          result.append("  ").append(I).append("   ").append(bytes).append("   ");
+          result.append(bytes / I).append("   ");
+          result.append(key);
+          result.append('\n');
+        }
       }
       return result.toString();
     }
