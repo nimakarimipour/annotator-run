932e0486f25b63496fe9e50edda59c6ee4ed2ca8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:44:50 -0700

    fix: 28 - DEREFERENCE_NULLABLE - System.err.println(rt.getName()); - dereferenced expression rt is @Nullable

--- src/main/java/com/ibm/wala/util/io/RtJar.java
+++ src/main/java/com/ibm/wala/util/io/RtJar.java
@@ -50,6 +50,10 @@ public class RtJar {
                   }
                 }));
 
-    System.err.println(rt.getName());
+    if (rt != null) {
+      System.err.println(rt.getName());
+    } else {
+      System.err.println("No suitable JarFile found.");
+    }
   }
 }
