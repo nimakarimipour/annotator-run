27a1357946432bb8bfd7eeafbcaaa0af0aa17116
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:29:24 -0700

    Revert "fix: 28 - DEREFERENCE_NULLABLE - System.err.println(rt.getName()); - dereferenced expression rt is @Nullable"
    This reverts commit e0e58703c16b56dc380e9b5fd2fbfa34458abefe.

--- src/main/java/com/ibm/wala/util/io/RtJar.java
+++ src/main/java/com/ibm/wala/util/io/RtJar.java
@@ -50,10 +50,6 @@ public class RtJar {
                   }
                 }));
 
-    if (rt != null) {
-      System.err.println(rt.getName());
-    } else {
-      System.err.println("RtJar is null");
-    }
+    System.err.println(rt.getName());
   }
 }
