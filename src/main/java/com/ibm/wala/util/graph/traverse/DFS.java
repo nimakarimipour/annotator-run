a1157f0349ef15f86dba234ff114d301b0b51732
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:32:46 -0700

    fix: 61 - UNBOX_NULLABLE - return (t1 - t2); - unboxing of a @Nullable value - t2

--- src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFS.java
@@ -131,7 +131,12 @@ public class DFS {
       }
       Integer t1 = order.get(o1);
       Integer t2 = order.get(o2);
-      // throws an exception if either node has not been ordered
+
+      // Ensure that neither t1 nor t2 is null before unboxing
+      if (t1 == null || t2 == null) {
+        throw new IllegalArgumentException("Both nodes must be ordered");
+      }
+
       return (t1 - t2);
     }
   }
