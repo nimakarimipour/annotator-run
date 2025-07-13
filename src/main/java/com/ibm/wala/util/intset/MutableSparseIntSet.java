a667a8b348cff0af9c29eebe9ac243830ac011eb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:33:20 -0700

    Revert "fix: 66 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit f7162cbadf4b32207f659e1fbca10d9da6c4f067.

--- src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
@@ -66,24 +66,23 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
   /** */
   @Override
   public boolean remove(int value) {
-    if (elements != null && size > 0) {
-      int removeIndex;
-      for (removeIndex = 0; removeIndex < size; removeIndex++) {
-        if (elements[removeIndex] >= value) {
+    if (elements != null) {
+      int remove;
+      for (remove = 0; remove < size; remove++) {
+        if (elements[remove] >= value) {
           break;
         }
       }
-      if (removeIndex == size) {
+      if (remove == size) {
         return false;
       }
-      if (elements[removeIndex] == value) {
+      if (elements[remove] == value) {
         if (size == 1) {
-          elements = new int[0]; // set to empty array instead of null
+          elements = null;
           size = 0;
         } else {
-          if (removeIndex < size) {
-            System.arraycopy(
-                elements, removeIndex + 1, elements, removeIndex, size - removeIndex - 1);
+          if (remove < size) {
+            System.arraycopy(elements, remove + 1, elements, remove, size - remove - 1);
           }
           size--;
         }
