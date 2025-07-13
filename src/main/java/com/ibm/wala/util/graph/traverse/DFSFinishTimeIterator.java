4108674bf3dcdf504aed5b557cae7ecd6d071344
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:44:25 -0700

    Revert "fix: 24 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field theNextElement (line 34) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit f408f3c1d9ad3750773fbebef668c3d26a65ada4.

--- src/main/java/com/ibm/wala/util/graph/traverse/DFSFinishTimeIterator.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/DFSFinishTimeIterator.java
@@ -15,7 +15,6 @@ import com.ibm.wala.util.collections.Iterator2Iterable;
 import com.ibm.wala.util.debug.UnimplementedError;
 import com.ibm.wala.util.graph.Graph;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -32,7 +31,7 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
   private static final long serialVersionUID = 8440061593631309429L;
 
   /** the current next element in finishing time order */
-  @Nullable private T theNextElement;
+  private T theNextElement;
 
   /** an enumeration of all nodes to search from */
   private Iterator<? extends T> roots;
@@ -95,8 +94,8 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
     }
     if (empty()) {
       T v = theNextElement;
-      setPendingChildren(Nullability.castToNonnull(v), getConnected(Nullability.castToNonnull(v)));
-      push(Nullability.castToNonnull(v));
+      setPendingChildren(v, getConnected(v));
+      push(v);
     }
     recurse:
     while (!empty()) {
@@ -106,15 +105,17 @@ public abstract class DFSFinishTimeIterator<T> extends ArrayList<T> implements I
         assert n != null : "null node in pc";
         Iterator<T> nChildren = getPendingChildren(n);
         if (nChildren == null) {
+          // found a new child: recurse to it.
           setPendingChildren(n, getConnected(n));
           push(n);
           continue recurse;
         }
       }
+      // the following saves space by allowing the original iterator to be GCed
       setPendingChildren(v, (Iterator<T>) EmptyIterator.instance());
 
-      while (getPendingChildren(Nullability.castToNonnull(theNextElement)) != null
-          && roots.hasNext()) {
+      // no more children to visit: finished this vertex
+      while (getPendingChildren(theNextElement) != null && roots.hasNext()) {
         theNextElement = roots.next();
       }
 
