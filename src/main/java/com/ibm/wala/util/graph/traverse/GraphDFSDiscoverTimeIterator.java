e84530cab263797cbbfd991fa6e283adab42a4d5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:44:51 -0700

    before:35to34 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression G is @Nullable' /home/nima/Developer/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/graph/traverse/GraphDFSDiscoverTimeIterator.java:34     return G.getSuccNodes(n);

--- src/main/java/com/ibm/wala/util/graph/traverse/GraphDFSDiscoverTimeIterator.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/GraphDFSDiscoverTimeIterator.java
@@ -30,7 +30,10 @@ abstract class GraphDFSDiscoverTimeIterator<T> extends DFSDiscoverTimeIterator<T
   }
 
   @Override
-  protected Iterator<? extends T> getConnected(T n) {
-    return G.getSuccNodes(n);
-  }
+    protected Iterator<? extends T> getConnected(T n) {
+      if (G == null) {
+        throw new IllegalStateException("Graph G has not been initialized.");
+      }
+      return G.getSuccNodes(n);
+    }
 }
