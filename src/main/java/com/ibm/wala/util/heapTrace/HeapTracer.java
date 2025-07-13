bd4f056c27e2589c9e56d5b7a19ee77362dbe5ff
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:41:59 -0700

    before:59to57 - Type='UNBOX_NULLABLE', message='unboxing of a @Nullable value - i1' /home/nima/Developer/nullness-benchmarks/wala-util/src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java:597         return i2 - i1;

--- src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
+++ src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
@@ -591,11 +591,13 @@ public class HeapTracer {
        * java.lang.Object)
        */
       @Override
-      public int compare(Object o1, Object o2) {
-        Integer i1 = sizeCount.get(o1);
-        Integer i2 = sizeCount.get(o2);
-        return i2 - i1;
-      }
+        public int compare(Object o1, Object o2) {
+          Integer i1 = sizeCount.get(o1);
+          Integer i2 = sizeCount.get(o2);
+          if (i1 == null) i1 = 0;
+          if (i2 == null) i2 = 0;
+          return i2 - i1;
+        }
     }
 
     /**
