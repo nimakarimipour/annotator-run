3857099215a43ea22dcce8b353599e9d61d0e2d0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:26:46 -0700

    Revert "fix: 2 - METHOD_NO_INIT - public SparseIntSet() { - initializer method does not guarantee @NonNull field elements (line 38) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 2cd0ecaed79113d7bd20a0f6c70dd78fe589ad86.

--- src/main/java/com/ibm/wala/util/intset/SparseIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/SparseIntSet.java
@@ -59,7 +59,7 @@ public class SparseIntSet implements IntSet {
 
   /** Subclasses should use this with extreme care. */
   public SparseIntSet() {
-    elements = new int[0];
+    elements = null;
     this.size = 0;
   }
 
