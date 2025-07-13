b9fa9a4e30e4591aa7674c1ce75377ea72d13d81
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:49:17 -0700

    Revert "fix: 56 - ASSIGN_FIELD_NULLABLE - private HashMap<Object, T> history = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit e409e8b554a297a360f6546223ca838fbbfcf178.

--- src/main/java/com/ibm/wala/util/graph/traverse/BFSPathFinder.java
+++ src/main/java/com/ibm/wala/util/graph/traverse/BFSPathFinder.java
@@ -14,7 +14,6 @@ import com.ibm.wala.util.collections.HashMapFactory;
 import com.ibm.wala.util.collections.HashSetFactory;
 import com.ibm.wala.util.collections.NonNullSingletonIterator;
 import com.ibm.wala.util.graph.Graph;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -139,7 +138,7 @@ public class BFSPathFinder<T> {
   }
 
   @Nullable private ArrayDeque<T> Q = null;
-  @Nullable private HashMap<Object, T> history = null;
+  private HashMap<Object, T> history = null;
 
   /**
    * @return a List of nodes that specifies the first path found from a root to a node accepted by
@@ -167,7 +166,7 @@ public class BFSPathFinder<T> {
       Iterator<? extends T> children = getConnected(N);
       while (children.hasNext()) {
         T c = children.next();
-        if (!Nullability.castToNonnull(history, "always initialized").containsKey(c)) {
+        if (!history.containsKey(c)) {
           Q.addLast(c);
           history.put(c, N);
         }
@@ -181,7 +180,7 @@ public class BFSPathFinder<T> {
    * @return a List which represents a path in the breadth-first search to Q[i]. Q holds the nodes
    *     visited during the BFS, in order.
    */
-  private List<T> makePath(T node, @Nullable Map<Object, T> history) {
+  private List<T> makePath(T node, Map<Object, T> history) {
     ArrayList<T> result = new ArrayList<>();
     T n = node;
     result.add(n);
