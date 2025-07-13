78ef449ad86633f33b21510ee74a382f504e3fcb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:41:51 -0700

    Revert "fix: 16 - DEREFERENCE_NULLABLE - return preds.get(N).iterator(); - dereferenced expression preds.get(N) is @Nullable"
    This reverts commit 950004c5ece8808d11958c26e1c73b197f8ff023.

--- src/main/java/com/ibm/wala/util/graph/GraphSlicer.java
+++ src/main/java/com/ibm/wala/util/graph/GraphSlicer.java
@@ -17,7 +17,6 @@ import com.ibm.wala.util.collections.IteratorUtil;
 import com.ibm.wala.util.debug.Assertions;
 import com.ibm.wala.util.graph.impl.GraphInverter;
 import com.ibm.wala.util.graph.traverse.DFS;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -273,7 +272,7 @@ public class GraphSlicer {
             if (!preds.containsKey(N)) {
               setPredNodes(N);
             }
-            return Nullability.castToNonnull(preds.get(N)).iterator();
+            return preds.get(N).iterator();
           }
 
           @Override
