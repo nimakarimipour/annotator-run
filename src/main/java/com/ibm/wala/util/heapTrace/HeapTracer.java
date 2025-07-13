43724f105e3187f8fc38255af5e157a60ae0fb27
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:27:10 -0700

    Revert "fix: 6 - DEREFERENCE_NULLABLE - if (d.getTotalSize() > 10000) { - dereferenced expression d is @Nullable"
    This reverts commit 64a84df3d387a4cc099dd6d13b79928472cdda8d.

--- src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
+++ src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
@@ -654,10 +654,8 @@ public class HeapTracer {
       int totalInstances = 0;
       int totalSize = 0;
       for (Demographics d : roots.values()) {
-        if (d != null) {
-          totalInstances += d.getTotalInstances();
-          totalSize += d.getTotalSize();
-        }
+        totalInstances += d.getTotalInstances();
+        totalSize += d.getTotalSize();
       }
       result.append("Total instances: ").append(totalInstances).append('\n');
       result.append("Total size(bytes): ").append(totalSize).append('\n');
@@ -667,11 +665,12 @@ public class HeapTracer {
       for (Field field : sortedDemo) {
         Object root = field;
         Demographics d = roots.get(root);
-        if (d != null && d.getTotalSize() > 10000) {
+        if (d.getTotalSize() > 10000) {
           result.append(" root: ").append(root).append('\n');
           result.append(d);
         }
       }
+
       return result.toString();
     }
 
