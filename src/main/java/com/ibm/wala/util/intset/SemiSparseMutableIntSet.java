a868ae0366f1c54875bc9f4402763949c0ffca8d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:46:47 -0700

    Revert "fix: 45 - DEREFERENCE_NULLABLE - int next = densePart.nextSetBit(i + 1); - dereferenced expression densePart is @Nullable"
    This reverts commit 56e9ee910adf4b2eb465c7ad38c6eee56a862900.

--- src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/SemiSparseMutableIntSet.java
@@ -340,14 +340,11 @@ public class SemiSparseMutableIntSet implements MutableIntSet {
 
       @Override
       public boolean hasNext() {
-        return densePart != null && densePart.nextSetBit(i + 1) != -1;
+        return densePart.nextSetBit(i + 1) != -1;
       }
 
       @Override
       public int next() {
-        if (densePart == null) {
-          throw new NullPointerException("densePart is null");
-        }
         int next = densePart.nextSetBit(i + 1);
         i = next;
         return next;
@@ -493,9 +490,6 @@ public class SemiSparseMutableIntSet implements MutableIntSet {
   }
 
   private boolean inDenseRange(int i) {
-    if (densePart == null) {
-      return false;
-    }
     return densePart.getOffset() <= i && densePart.length() > i;
   }
 
