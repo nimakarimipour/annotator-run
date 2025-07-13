b7282e5081b22c3ab66bc540efd2c9c8048f5483
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:33:06 -0700

    fix: 64 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseIntSet.java
@@ -165,13 +165,11 @@ public class MutableSparseIntSet extends SparseIntSet implements MutableIntSet {
     if (that instanceof SparseIntSet) {
       SparseIntSet set = (SparseIntSet) that;
       if (set.elements != null) {
-        // SJF: clone is performance problem. don't use it.
-        // elements = set.elements.clone();
         elements = new int[set.elements.length];
         System.arraycopy(set.elements, 0, elements, 0, set.size);
         size = set.size;
       } else {
-        elements = null;
+        elements = new int[0]; // Initialize with an empty array instead of null
         size = 0;
       }
     } else {
