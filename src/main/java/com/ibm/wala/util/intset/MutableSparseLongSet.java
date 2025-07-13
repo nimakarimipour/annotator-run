f3f57ba2a4f89642363f24e2e1d952dd8c7ae976
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:28:51 -0700

    fix: 23 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
@@ -77,7 +77,7 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
       }
       if (elements[remove] == value) {
         if (size == 1) {
-          elements = null;
+          elements = new long[0]; // Replace null with an empty array
           size = 0;
         } else {
           if (remove < size) {
