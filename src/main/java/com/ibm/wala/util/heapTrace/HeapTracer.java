0f1e7e30da128fa28082bad95c70e41aa3812a94
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:29:12 -0700

    Revert "fix: 26 - UNBOX_NULLABLE - return i2 - i1; - unboxing of a @Nullable value - i2"
    This reverts commit 56000577b6152b7833fbf8362a227b4d7783e61c.

--- src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
+++ src/main/java/com/ibm/wala/util/heapTrace/HeapTracer.java
@@ -594,9 +594,7 @@ public class HeapTracer {
       public int compare(Object o1, Object o2) {
         Integer i1 = sizeCount.get(o1);
         Integer i2 = sizeCount.get(o2);
-        int value1 = (i1 == null) ? 0 : i1;
-        int value2 = (i2 == null) ? 0 : i2;
-        return value2 - value1;
+        return i2 - i1;
       }
     }
 
