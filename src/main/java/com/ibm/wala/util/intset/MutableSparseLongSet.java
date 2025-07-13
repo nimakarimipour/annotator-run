98a071939d1357912c4b86a74ebf5dd034c8c97a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:31:33 -0700

    fix: 49 - ASSIGN_FIELD_NULLABLE - elements = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
+++ src/main/java/com/ibm/wala/util/intset/MutableSparseLongSet.java
@@ -182,19 +182,18 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
     if (this.isEmpty()) {
       return;
     } else if (that.isEmpty()) {
-      elements = null;
+      elements = new long[0];
       size = 0;
       return;
     } else if (this.equals(that)) {
       return;
     }
 
-    // some simple optimizations
     if (size == 1) {
       if (that.contains(elements[0])) {
         return;
       } else {
-        elements = null;
+        elements = new long[0];
         size = 0;
         return;
       }
@@ -208,7 +207,7 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
         elements[0] = that.elements[0];
         return;
       } else {
-        elements = null;
+        elements = new long[0];
         size = 0;
         return;
       }
@@ -220,20 +219,19 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
     long[] br = that.elements;
     int bi = 0;
     int bl = that.size;
-    long[] cr = null; // allocate on demand
-    int ci = 0;
+    long[] cr = null;
 
+    int ci = 0;
     while (ai < al && bi < bl) {
       long cmp = (ar[ai] - br[bi]);
 
-      // (accept element only on a match)
-      if (cmp > 0) { // a greater
+      if (cmp > 0) {
         bi++;
-      } else if (cmp < 0) { // b greater
+      } else if (cmp < 0) {
         ai++;
       } else {
         if (cr == null) {
-          cr = new long[al]; // allocate enough (i.e. too much)
+          cr = new long[al];
         }
         cr[ci++] = ar[ai];
         ai++;
@@ -241,9 +239,8 @@ public final class MutableSparseLongSet extends SparseLongSet implements Mutable
       }
     }
 
-    // now compact cr to 'just enough'
     size = ci;
-    elements = cr;
+    elements = cr != null ? cr : new long[0];
     return;
   }
 
